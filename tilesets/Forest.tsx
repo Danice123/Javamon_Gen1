67dfcb985079446a64e92ee5a37b2969d8af878c
Daniel Mitchell <mitchelldanice@gmail.com>
2019-08-13 22:09:31 -0400

    Progress until Post Brock finished

---
+++ tilesets/Forest.tsx
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Forest" tilewidth="16" tileheight="16" tilecount="128" columns="16">
+ <image source="forest.png" width="256" height="128"/>
+ <tile id="16">
+  <properties>
+   <property name="Encounter" value="Grass"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="Encounter" value="Grass"/>
+  </properties>
+  <animation>
+   <frame tileid="16" duration="1000"/>
+   <frame tileid="17" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="Encounter" value="Grass"/>
+  </properties>
+  <animation>
+   <frame tileid="16" duration="1000"/>
+   <frame tileid="18" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="Encounter" value="Grass"/>
+  </properties>
+  <animation>
+   <frame tileid="16" duration="1000"/>
+   <frame tileid="19" duration="300"/>
+  </animation>
+ </tile>
+</tileset>
